d8db6c834d1509a2e48e3239d111cd3911757b75
Jackson Bale <bale0837@vandals.uidaho.edu>
2026-09-23 16:56:25 +0000

    Add a function to calculate the time step

--- Diffusion_model.py
+++ Diffusion_model.py
@@ -2,12 +2,16 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+def calculate_stable_time_step(grid_spacing,diffusivity):
+    return 0.5 * grid_spacing**2 / diffusivity
+
 def plot_profile(xvals,yvals,color='red',title=None, outfile=None):
     plt.figure()
     plt.plot(xvals,yvals, color)
     plt.xlabel("distance")
     plt.ylabel("elevation")
     plt.title(title)
+    plt.savefig(outfile)
     
 
 D = 100
@@ -26,7 +30,7 @@ z[x > Lx / 2] = z_lo
 plot_profile(x,z,color='r', title='Initial Hillslope Profile', outfile='Initial _profile.png')
 
 nt = 5000
-dt = 0.5 * dx**2 / D
+dt= calculate_stable_time_step(dx,D)
 
 for _ in range(0, nt):
     z[1:-1] += D * dt / dx**2 * (z[:-2] - 2*z[1:-1] + z[2:])
