8695a012ad06083bf8847d45bc73483598b4338a
Jackson Bale <bale0837@vandals.uidaho.edu>
2026-09-24 17:21:16 +0000

    Put gaurds on title and outfile keywords

--- Diffusion_model.py
+++ Diffusion_model.py
@@ -10,30 +10,34 @@ def plot_profile(xvals,yvals,color='red',title=None, outfile=None):
     plt.plot(xvals,yvals, color)
     plt.xlabel("distance")
     plt.ylabel("elevation")
-    plt.title(title)
-    plt.savefig(outfile)
+    if title is not None:
+        plt.title(title)
+    if outfile is not None:
+        plt.savefig(outfile)
     
 
-D = 100
-Lx = 300
-
-dx = 0.5
-x = np.arange(start=0, stop=Lx, step=dx)
-nx=len(x)
-
-z = np.zeros_like(x)
-z_hi = 500.0
-z_lo=0.0
-z[x <= Lx / 2] = z_hi
-z[x > Lx / 2] = z_lo
-
-plot_profile(x,z,color='r', title='Initial Hillslope Profile', outfile='Initial _profile.png')
-
-nt = 5000
-dt= calculate_stable_time_step(dx,D)
-
-for _ in range(0, nt):
-    z[1:-1] += D * dt / dx**2 * (z[:-2] - 2*z[1:-1] + z[2:])
-
-plot_profile(x,z,color='b', title='Final Hillslope Profile',  outfile='Final _profile.png')
+if __name__ == "__main__":
+    
+    D = 100
+    Lx = 300
+    
+    dx = 0.5
+    x = np.arange(start=0, stop=Lx, step=dx)
+    nx=len(x)
+    
+    z = np.zeros_like(x)
+    z_hi = 500.0
+    z_lo=0.0
+    z[x <= Lx / 2] = z_hi
+    z[x > Lx / 2] = z_lo
+    
+    plot_profile(x,z,color='r', title='Initial Hillslope Profile', outfile='Initial _profile.png')
+    
+    nt = 5000
+    dt= calculate_stable_time_step(dx,D)
+    
+    for _ in range(0, nt):
+        z[1:-1] += D * dt / dx**2 * (z[:-2] - 2*z[1:-1] + z[2:])
+    
+    plot_profile(x,z,color='b', title='Final Hillslope Profile',  outfile='Final _profile.png')
 
